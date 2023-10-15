1cdcccd03af48ab36144c606b37240e983c9ec4d
VenkataPraveen-09 <2000031043cse@gmail.com>
2023-10-16 02:30:02 +0530

    new file:   frontend/src/components/Breadcrums/Breadcrum.css 	new file:   frontend/src/components/Breadcrums/Breadcrum.jsx 	modified:   frontend/src/components/Item/Item.jsx 	new file:   frontend/src/components/ProductDisplay/ProductDisplay.css 	new file:   frontend/src/components/ProductDisplay/ProductDisplay.jsx 	modified:   frontend/src/pages/Product.jsx

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import { useParams } from 'react-router-dom';
+import { ShopContext } from '../Context/ShopContext'
+import Breadcrum from '../components/Breadcrums/Breadcrum';
+import ProductDisplay from '../components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+  const {all_product}=useContext(ShopContext);
+  const { productId } = useParams();
+
+  // Find the product with a matching id
+  const product = all_product.find((e)=>e.id===Number(productId));
+
   return (
     <div>
-      
+      {/* {/* {product ? (
+        <div>
+          <h1>{product.name}</h1>
+          <p>Price: ${product.new_price}</p>
+          <p>Old Price: ${product.old_price}</p>
+          <img src={product.image} alt={product.name} />
+        </div>
+      ) : (
+        <p>Product not found</p> 
+      )} */}
+      <Breadcrum product={product}/>
+      <ProductDisplay product={product}/>
     </div>
-  )
+  );
 }
 
-export default Product
+export default Product;
+ 
